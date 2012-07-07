856808534e1286e03c895333862c5d4e52f96068
Hans Christian Reinl <anything@drublic.de>
2012-07-07 15:59:49 +0200

    Modeller: init function for jsPlumb, PackageBuilder and PackageBuilder.Modeller have to be defined, passing jsPlumb to PackageBuilder.Modeller instead of global

--- Resources/Public/JavaScript/jsplumb/init.js
+++ Resources/Public/JavaScript/jsplumb/init.js
@@ -1,6 +1,12 @@
 /*jshint curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, sub: true, undef: true, boss: true, eqnull: true, browser: true */
 /*globals jQuery, $, TYPO3 */
-TYPO3.PackageBuiler.Modeller._plumb = {
+
+// Init PackageBuilder and PackageBuilder.Modeller
+TYPO3.PackageBuilder = {};
+TYPO3.PackageBuilder.Modeller = {};
+
+// Set some initial values for jsPlumb
+TYPO3.PackageBuilder.Modeller._plumb = {
 	colors : {
 		connector_stroke : "rgba(100, 100, 100, 1)",
 		connector_stroke_highlight : "rgba(200, 200, 200, 1)",
@@ -12,4 +18,27 @@ TYPO3.PackageBuiler.Modeller._plumb = {
 			fillStyle: "#a7b04b"
 		}
 	}
+};
+
+// Set jsPlumb to be part of Modeller package rather than global varibale
+TYPO3.PackageBuilder.Modeller.jsPlumb = window.jsPlumb;
+
+// Init function for Modeller
+TYPO3.PackageBuilder.Modeller.init = function () {
+
+	// Drag-events for components
+	var timestamp = 0;
+
+	$(document).on('drag', '.modeller-stage .component', function (e) {
+
+		// Check if timestemps differ more then 100ms
+		if ((e.timeStamp - timestamp) > 100) {
+			var offset = $(this).offset();
+
+			// @TODO Map position to model's properties
+
+			timestamp = e.timeStamp;
+		}
+	});
+
 };
