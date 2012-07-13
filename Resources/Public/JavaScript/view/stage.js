2a751cd1fba8d7114076ba71d9521dbc66a9d345
Hans Christian Reinl <anything@drublic.de>
2012-07-13 10:58:28 +0200

    Add models-collection (ArrayProxy) and template for component

--- Resources/Public/JavaScript/view/stage.js
+++ Resources/Public/JavaScript/view/stage.js
@@ -1,28 +1,49 @@
 /*jshint curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, sub: true, undef: true, boss: true, eqnull: true, browser: true */
-/*globals jQuery, $, TYPO3 */
+/*globals jQuery, $, TYPO3, Ember */
 (function() {
+	TYPO3.PackageBuilder.Modeller.Controller = Ember.ArrayProxy.create({
+		content: [],
+		createModel: function (model) {
+			var _model = TYPO3.Ice.Model.Element.create(model);
+			this.pushObject(_model);
+			return _model;
+		}
+	});
 
 	TYPO3.Ice.View.StageClass = TYPO3.Ice.View.StageClass.extend({
-	didInsertElement: function() {
-		var object2table;
-			object2table = function(o) {
-				var key, t;
-				t = "<div class=\"component\">";
-				for (key in o) {
-					t += "<tr><td class=\"key\">" + key + "</td><td class=\"value\">";
-					if (typeof o[key] === "object") {
-						t += object2table(o[key]);
-					} else {
-						t += o[key];
-					}
-					t += "</tr>";
+		templateName: 'Modeller-Component',
+
+		componentObj: function(o) {
+			var key,
+				component = {
+					label: o['label'],
+					identifier: o['identifier'],
+					props: []
+				};
+
+			for (key in o) {
+				if (key === "label" || key === "identifier") {
+					continue;
 				}
-				t += "</div>";
-				return t;
-			};
-			//window.console.log(TYPO3.Ice.Utility.convertToSimpleObject(this.get("projectDefinition")));
+
+				if (typeof o[key] === "object") {
+					// @TODO define how objects in objects look
+					TYPO3.PackageBuilder.Modeller.Controller.createModel(this.componentObj(o[key]));
+				} else {
+					component.props.push({
+						key: key,
+						value: o[key]
+					});
+				}
+			}
+
+			return component;
+		},
+
+		didInsertElement: function() {
 			if (this.getPath("projectDefinition")) {
-				return this.$().html("<div class='stage'><h3>Model Definition</h3>" + object2table(TYPO3.Ice.Utility.convertToSimpleObject(this.get("projectDefinition"))) + '</div>');
+				var _model = this.componentObj(TYPO3.Ice.Utility.convertToSimpleObject(this.get('projectDefinition')));
+				TYPO3.PackageBuilder.Modeller.Controller.createModel(_model);
 			}
 		}.observes('projectDefinition.__nestedPropertyChange')
 	});
