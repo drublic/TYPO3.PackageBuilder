2dcdd3d28e01f1790f9264eeb3aaaf3e50a60f41
Hans Christian Reinl <anything@drublic.de>
2012-09-28 14:30:19 +0200

    Accordion is now settings only

--- Resources/Public/JavaScript/view/settings.js
+++ Resources/Public/JavaScript/view/settings.js
@@ -2,24 +2,27 @@
 
 	"use strict";
 
-	// Create a view which will be the accordion in the right sidebar
-	TYPO3.Ice.View.Accordion = Ember.View.extend({
+	// Create a view which will hold the settings in the right sidebar
+	TYPO3.Ice.View.Settings = Ember.View.extend({
 
 		relationLabelsBinding: "TYPO3.PackageBuilder.modellerBuild.settings.showRelationLabels",
+		zoomBinding: "TYPO3.PackageBuilder.modellerBuild.settings.zoom",
 
 		// The template
-		templateName: "Modeller-Accordion",
+		templateName: "Modeller-Settings",
 
 		// Binding for the current element
 		projectDefinitionBinding: "TYPO3.Ice.Model.Project.projectDefinition",
 
-		// If the project definition changes
-		onProjectElementChange: function () {
-		}.observes('projectDefinition'),
-
 		relationLabelsChange: function () {
 			this.set('relationLabels', !this.get('relationLabels'));
 			TYPO3.PackageBuilder.modellerBuild.set('settings.showRelationLabels', this.get('relationLabels'));
+		},
+
+		zoomChange: function () {
+			this.set('zoom', !this.get('zoom'));
+			TYPO3.PackageBuilder.modellerBuild.set('settings.zoom', this.get('zoom'));
 		}
 	});
+
 }());
